ff72857d125d622dc68a4bfff30a9fac9350a054
Sandip Ghadge <sandip.ghadge@wwindia.com>
2020-03-28 15:58:30 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import socketIOClient from "socket.io-client";
+import React, { Component } from 'react';
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      response: true,
+      endpoint: "http://127.0.0.1:3005?token='testtoken'"
+    };
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  componentDidMount() {
+    const { endpoint } = this.state;
+    const socket = socketIOClient(endpoint);
+    socket.on("onMessageSent", data => this.setState({ response: data }));
+  }
+
+  render() {
+    const { response } = this.state;
+    return (
+        <div style={{ textAlign: "center" }}>
+          {response
+              ? <p>
+                The temperature in Florence is: {response} °F
+              </p>
+              : <p>Loading...</p>}
+        </div>
+    );
+  }
 }
 
 export default App;
